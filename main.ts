499fecf843eee7efbcee0a1f89837eac5c19ef55
SolarDragon8882 <56969485+SolarDragon8882@users.noreply.github.com>
2020-09-11 09:01:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    music.playMelody("C5 C E G F A D C5 ", 500)
+    music.playMelody("C5 B A G F E D E ", 500)
+    music.playMelody("G E C B F C5 D A ", 500)
+    music.playMelody("D G E B F C5 A C ", 500)
+    music.playMelody("D G E B F C5 A C ", 500)
+    basic.showString("Hello!")
+    basic.showString("welcome")
+    basic.showString("Noah")
+})
